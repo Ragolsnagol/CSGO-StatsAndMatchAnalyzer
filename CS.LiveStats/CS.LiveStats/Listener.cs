f161b8a2f6cf65a1d630a6baa8cf4ba8bc750eaa
Ragolsnagol <ragolsnagol@gmail.com>
2017-09-20 21:20:19 -0400

    Added the first project for reading the game state.

---
+++ CS.LiveStats/CS.LiveStats/Listener.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CSGSI;
+
+namespace CS.LiveStats
+{
+    public class Listener
+    {
+        private GameStateListener _listener;
+        private GameState _currentGS;
+        private int _health;
+
+        public GameState CurrentGS
+        {
+            get { return _currentGS; }
+        }
+
+        public Listener()
+        {
+            _listener = new GameStateListener(3000);
+            _listener.NewGameState += new NewGameStateHandler(OnNewGameState);
+
+            // If the listener can't start then abort.
+            if (_listener.Start() == false)
+            {
+                Environment.Exit(0);
+            }
+            Console.WriteLine("Listening...");
+        }
+
+        private void OnNewGameState(GameState gs)
+        {
+            // First thing to do is set the current gamestate so other classes can use it.
+            _currentGS = gs;
+
+            _health = gs.Player.State.Health;
+
+            Console.WriteLine(_health);
+        }
+
+    }
+}
---
+++ CS.LiveStats/CS.LiveStats/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CSGSI;
+using CSGSI.Nodes;
+
+namespace CS.LiveStats
+{
+    class Program
+    {
+        static private Listener _listener;
+
+        static GameStateListener gsl;
+
+        static void Main(string[] args)
+        {
+            _listener = new Listener();
+        }
+    }
+}
